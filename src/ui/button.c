fc87ab9d7e78548a0c540f676ad52a683ad94e3f
RAFAEL COUTINHO LIMA <rafaelcoutinholima12345@gmail.com>
2025-11-06 09:56:37 -0300

    terminei a parte dos botões

---
+++ src/ui/button.c
@@ -0,0 +1,57 @@
+#include "button.h"
+#include "raylib.h"
+
+Button CreateButton(float x,float y,float width,float height,const char *text){
+    Button button= {0};
+    //zera todos os valores do butão 
+    float posX = (x<0)?(GetScreenWidth()-width)/2.0f :x;
+    float posY = (y<0)?(GetScreenHeight()-height)/2.0f:y;
+    //faz a verificação e centraliza os limites de x e y
+    button.bounds = (Rectangle){posX,posY,width,height};
+    //pegando as dimensões do botão 
+    button.text= text;
+
+    button.baseColor=(Color){60,60,60,255};//cinza esscuro
+    button.hoverColor=(Color){100,100,100,255};//cinza medio
+    button.clickColor=(Color){30,30,30,255};//cinza escuro
+
+    button.hovered =false;
+    button.clicked=false;
+    return button;
+}
+bool UpdateButton(Button *button){
+    Vector2 mousePoint=GetMousePosition();
+    button->hovered=false;
+    button->clicked=false;
+    //resetando o botão
+    bool clickedThisFrame=false;
+    if(CheckCollisionPointRec(mousePoint,button->bounds)){
+        button->hovered=true;
+        if(IsMouseButtonDown(MOUSE_LEFT_BUTTON)){
+            button->clicked=true;//feedback se foi pressionado        
+        }
+        if(IsMouseButtonReleased(MOUSE_LEFT_BUTTON)){
+            clickedThisFrame=true;//confirmar o click
+        }
+        return clickedThisFrame;
+    }
+}
+void DrawButton(Button button){
+    Color color=button.baseColor;
+    if(button.hovered){
+        color=button.hoverColor;//a cor de quando ta em cima o mouse
+    }
+    if(button.clicked){
+        color=button.clickColor;//cor de quando clica
+    }
+    DrawRectangleRec(button.bounds,color);
+    //desenhar o botão
+    DrawRectangleLinesEx(button.bounds,2,button.hoverColor);// as bordas dos botões
+    //botar o texto no centro
+    int fontSize=20;
+    int textWidth =MeasureText(button.text,fontSize);
+    float textX=button.bounds.x +(button.bounds.width-textWidth)/2.0f;
+    float textY =button.bounds.y+(button.bounds.height-fontSize)/2.0f;
+    
+    DrawText(button.text,(int)textX,(int)textY,fontSize,WHITE);
+}
---
+++ src/ui/button.h
@@ -0,0 +1,22 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#include "raylib.h"
+
+typedef struct{
+    Rectangle bounds;//guarda o tamanho altura ,essas coisas
+    const char *text;
+    Color baseColor;
+    Color hoverColor;
+    Color clickColor;
+    bool hovered;
+    bool clicked; //feedback visual do botão
+}Button;
+
+Button CreateButton(float x,float y ,float width,float height,const char *text);
+//criando o botão 
+bool UpdateButton(Button *button);
+//para atualizar o estado do botão e saber se parou de precionar e tudo  mais 
+void DrawButton(Button button);
+
+#endif
