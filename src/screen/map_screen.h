a8757da7bfc8617d27d9fb0f95777d4c4461e24c
RAFAEL COUTINHO LIMA <rafaelcoutinholima12345@gmail.com>
2025-11-19 11:10:47 -0300

    ajustando o map.h para tirar o botoes depois e linkar com a imagem da home de fase com a  struct

--- src/screen/map_screen.h
+++ src/screen/map_screen.h
@@ -2,6 +2,21 @@
 #define MAP_SCREEN_H
 
 #include "core/state.h"
+#include "raylib.h"
 
+#define  MAX_MAP_NODES 10
+typedef enum {
+    NODE_LOCKED,  //bloquear fase
+    NODE_AVAILABLE,//fase disponivel
+    NODE_COMPLETED,//fase concluido 
+}NodeState;
+
+typedef struct {
+    int levelId;//ID que vai ser passado para o level_loader
+    Vector2 position;//posição x e y 
+    NodeState state;//estado atual
+    bool isHovered;//se o mouse ta em cima
+    float animScale; //a animação pular
+}LevelNode;
 GameState map_screen_state(void);
 #endif
